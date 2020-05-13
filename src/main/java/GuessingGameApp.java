9aba4344c5d17808011e3d339cc5cdbf25013ff0
Gale King <grexkingiv@gmail.com>
2020-05-12 19:32:29 -0500

    First iteration is complete

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to the Guessing Game!");
+        System.out.println("Please input a number between 1 and 10");
+        Scanner input = new Scanner(System.in );
+        int guess = input.nextInt();
+        int numberOfAttempts = 0;
 
+
+
+        if(guess == 7){
+            System.out.println("Congratulations you have won!");
+        } else{System.out.println("Sorry, you lose");}
+
+        if (numberOfAttempts < 2)
+            {
+                numberOfAttempts++;
+            System.out.println("You may attempt one more guess");
+            guess = input.nextInt();
+                if(guess == 7) {
+                    System.out.println( "Congratulations you have won!" );
+                } else{System.out.println("Sorry, you lose");}
+        }
+
+
+
+
+        if(guess ==0){
+            System.out.println("Please input a number between 1 and 10 or enter -1 to exit the program.");
+        }
     }
 
 }
